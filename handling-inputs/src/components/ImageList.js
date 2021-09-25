9d97593db1bff1fff67b87a42cf89bca74a41f44
catchmeprateek <guptaprateek10@gmail.com>
2021-09-25 14:56:58 +0530

    .

--- handling-inputs/src/components/ImageList.js
+++ handling-inputs/src/components/ImageList.js
@@ -3,6 +3,7 @@ import React from "react";
 import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
+  console.log(props);
   const imagesf = props.images.map((image) => {
     return <ImageCard key={image.id} image={image} />;
   });
