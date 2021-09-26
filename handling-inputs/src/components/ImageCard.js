cec2b8a5745e6d211d93c50ae6f5a0905cb8bec6
catchmeprateek <guptaprateek10@gmail.com>
2021-09-26 18:44:08 +0530

    Final code savePoint

--- handling-inputs/src/components/ImageCard.js
+++ handling-inputs/src/components/ImageCard.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.imageRef = React.createRef();
+  // }
+  // componentDidMount() {
+  //   this.imageRef.current.addEventListner("load", this.setSpans);
+  // }
+  // setSpans = () => {
+  //   console.log(this.imageRef.current.clientHeight);
+  // };
+
   render() {
+    const { description, urls } = this.props.image;
     return (
       <div>
-        <img
-          alt={this.props.image.description}
-          src={this.props.image.urls.regular}
-        />
+        <img ref={urls.regular} alt={description} src={urls.regular} />
       </div>
     );
   }
