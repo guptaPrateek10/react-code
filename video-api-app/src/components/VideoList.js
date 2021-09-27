2fe439f2094d134878775864c5e6a2ae4a452ab5
catchmeprateek <guptaprateek10@gmail.com>
2021-09-27 21:36:55 +0530

    Final Code

--- video-api-app/src/components/VideoList.js
+++ video-api-app/src/components/VideoList.js
@@ -4,7 +4,13 @@ import VideoItem from "./VideoItem";
 const VideoList = ({ videos, onVideoSelect }) => {
   //here we will use props.videos
   const renderedList = videos.map((video) => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return (
+      <VideoItem
+        Key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
 
   return <div className="ui relaxed divided list">{renderedList} </div>;
